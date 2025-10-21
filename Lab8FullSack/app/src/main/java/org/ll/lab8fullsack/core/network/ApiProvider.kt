239c2a2444ebd6cc30a2b865100e4d121254c3e6
Luisa Lamprea <llamprea@hotmail.com>
2025-10-20 20:42:11 -0500

    lab 8 documentation

--- Lab8FullSack/app/src/main/java/org/ll/lab8fullsack/core/network/ApiProvider.kt
+++ Lab8FullSack/app/src/main/java/org/ll/lab8fullsack/core/network/ApiProvider.kt
@@ -23,3 +23,38 @@ object ApiProvider {
 // two ways of define it:
 //private val retrofit: Retrofit by lazy {
 //private val retrofit: Lazy<Retrofit> = lazy {
+
+/*
+
+Retrofit is the library (for javaand android) that alows API comunication. it authomatically writtes implements the interface (so it HAS to bean interface)
+
+Retrofit API definiion looks like that
+@GET @POST  define the callingmathod
+@Body to besent in the ody
+@path ???
+{} to be replaced by avalue
+@query   to be send in querystring 
+
+interface UserApiService {
+@POST("users")
+suspend fun createUser(@Body userRequestDto: UserRequestDto): UserResponseDto
+@GET("users")
+suspend fun getUsers(): List<UserResponseDto>
+@GET("users/{id}")
+suspend fun getUserById(@Path("id") id: Int): UserResponseDto
+@GET("users")
+suspend fun getUsersSortedByDescending(@Query("sort") sortOrder: String = "desc"): List<UserResponseDto>
+@GET("users")
+suspend fun getFilteredUsers(
+@Query("sort") sortOrder: String = "desc",
+@Query("page") page: Int = 1,
+@Query("limit") limit: Int = 10,
+@Query("role") role: String? = null
+): List<UserResponseDto>
+}
+GET /users?sort=asc&page=1&limit=10&role=admin
+
+
+example of showing pictures using Retrofit
+https://kotlinlang.org/docs/serialization.html#example-json-serialization
+*/
